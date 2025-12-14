98fc066a803215e121d1a3a75ed119da03f7e415
Alex Haydock <alex@alexhaydock.co.uk>
2025-12-14 13:28:27 +0000

    Attempted build

--- .github/podman-move-storage.sh
+++ .github/podman-move-storage.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /etc/containers
-sudo mkdir -p /mnt/podman
+sudo mkdir -p \
+  /etc/containers \
+  /mnt/podman/graphroot \
+  /mnt/podman/rl_storage
 
-# We can't change the runroot since /mnt needs certain sticky
-# bits and can't have noexec etc etc
+# We can't change the runroot away from the default, 
+# since /mnt needs certain sticky bits and can't have
+# noexec etc etc
 sudo tee /etc/containers/storage.conf <<'EOF'
 [storage]
 driver = "overlay"
-graphroot = "/mnt/podman"
+runroot = "/run/containers/storage"
+graphroot = "/mnt/podman/graphroot"
+rootless_storage_path = "/mnt/podman/rl_storage"
 EOF
 
 sudo podman info --debug | grep -E 'graphRoot|runRoot'
