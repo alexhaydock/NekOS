256150c93f417d939a893db13f550ffeb2ccd97e
Alex Haydock <alex@alexhaydock.co.uk>
2025-12-14 13:07:39 +0000

    Attempted build

--- .github/podman-move-storage.sh
+++ .github/podman-move-storage.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 sudo mkdir -p /etc/containers
-sudo mkdir -p /mnt/podman
+sudo mkdir -p /mnt/podman/graphroot
+sudo mkdir -p /mnt/podman/runroot
 
 sudo tee /etc/containers/storage.conf <<'EOF'
 [storage]
 driver = "overlay"
-graphroot = "/mnt/podman"
+graphroot = "/mnt/podman/graphroot"
 runroot = "/mnt/podman/runroot"
 EOF
 
 sudo podman info --debug | grep -E 'graphRoot|runRoot'
+
+sudo chown -R "${RUNNER_USER}":"${RUNNER_USER}" /mnt/podman
