90b4cc67d3e3b948b4da8af692160a0fc24be856
Alex Haydock <alex@alexhaydock.co.uk>
2025-12-18 01:09:02 +0000

    Add first reproducible userland and UKI

--- uki/build.sh
+++ uki/build.sh
@@ -15,4 +15,31 @@ podman build --target final -t uki .
 #podman run --rm -it uki
 
 # Run container to copy UKI into output dir
+podman run --rm -it -v "$(pwd)/build:/opt/out:Z" --entrypoint cp uki -fv /opt/uki.unsigned.efi /opt/out/uki.unsigned.efi
 podman run --rm -it -v "$(pwd)/build:/opt/out:Z" --entrypoint cp uki -fv /opt/uki.efi /opt/out/uki.efi
+
+# Validate build hash based on architecture
+# (we do this for the unsigned UKI only since the signed one
+# depends on cryptographic private keys which independent
+# verifiers aren't going to be in possession of)
+arch="$(uname -m)"
+case "$arch" in
+    "x86_64" | "amd64")
+        if ! echo '518d5f671ba6e8df3fa14031aaf22c903e16c451fe2b18b2254c0eab11bc7a14  build/uki.unsigned.efi' \
+            | sha256sum -c; then
+            echo 'Build does not match expected checksum!'
+            exit 1
+        else
+            echo 'Build matches expected checksum!'
+        fi
+    ;;
+    "aarch64" | "arm64")
+        if ! echo '0000000000000000000000000000000000000000000000000000000000000000  build/uki.unsigned.efi' \
+            | sha256sum -c; then
+            echo 'Build does not match expected checksum!'
+            exit 1
+        else
+            echo 'Build matches expected checksum!'
+        fi
+    ;;
+esac
--- userland/build.sh
+++ userland/build.sh
@@ -11,6 +11,29 @@ podman build --target final -t ul .
 podman run --rm -it -v "$(pwd)/build:/opt/out:Z" --entrypoint cp ul -fv /opt/initramfs /opt/out/initramfs
 podman run --rm -it -v "$(pwd)/build:/opt/out:Z" --entrypoint cp ul -fv /opt/initramfs.txt /opt/out/initramfs_filelist.txt
 
+# Validate build hash based on architecture
+arch="$(uname -m)"
+case "$arch" in
+    "x86_64" | "amd64")
+        if ! echo '0130b05adcc46b5c39f1934390704ffcf7d0829e9d962a072a5767aacfaa68ad  build/initramfs' \
+            | sha256sum -c; then
+            echo 'Build does not match expected checksum!'
+            exit 1
+        else
+            echo 'Build matches expected checksum!'
+        fi
+    ;;
+    "aarch64" | "arm64")
+        if ! echo '0000000000000000000000000000000000000000000000000000000000000000  build/initramfs' \
+            | sha256sum -c; then
+            echo 'Build does not match expected checksum!'
+            exit 1
+        else
+            echo 'Build matches expected checksum!'
+        fi
+    ;;
+esac
+
 # echo reminder
 echo ""
 echo "Userland built"
